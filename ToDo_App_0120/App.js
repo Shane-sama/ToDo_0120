4dcb7e16262765203a4b2e55e51f3f1ed7a16722
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2023-11-23 11:22:56 +0100

    ToDo App
    ToDo App überarbeitet

--- ToDo_App_0120/App.js
+++ ToDo_App_0120/App.js
@@ -1,26 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Button from './components/Button';
+const MainScreen = ({ navigation, todos, setTodos }) => {
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
-//<Text style={{ color: '#fff' }}>Hoi</Text>
-export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="test" />
-        <Button theme="add" label="Add ToDo" />
-      </View>
-      <StatusBar style="auto" />
+      <Text>Main Screen</Text>
+      {todos.map((todo, index) => (
+        <TouchableOpacity key={index} onPress={() => deleteTodo(index)}>
+          <Text>{todo}</Text>
+        </TouchableOpacity>
+      ))}
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => navigation.navigate('AddToDo')}
+      >
+        <Text>Add ToDo</Text>
+      </TouchableOpacity>
     </View>
+  );
+};
+
+const AddToDoScreen = ({ navigation, todos, setTodos }) => {
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      const newTodos = [...todos, newTodo];
+      setTodos(newTodos);
+      navigation.navigate('Main');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Add ToDo Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter ToDo"
+        onChangeText={(text) => setNewTodo(text)}
+      />
+      <TouchableOpacity
+        style={styles.confirmButton}
+        onPress={addTodo}
+      >
+        <Text>Confirm</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          options={{
+            title: 'ToDo App',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => setTodos([])}>
+                <Text style={{ marginRight: 20 }}>Clear All</Text>
+              </TouchableOpacity>
+            ),
+          }}
+        >
+          {(props) => <MainScreen {...props} todos={todos} setTodos={setTodos} />}
+        </Stack.Screen>
+        <Stack.Screen
+          name="AddToDo"
+          options={{ title: 'Add ToDo' }}
+        >
+          {(props) => <AddToDoScreen {...props} todos={todos} setTodos={setTodos} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButton: {
+    backgroundColor: 'lightblue',
+    padding: 10,
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginTop: 20,
+    width: '80%',
+  },
+  confirmButton: {
+    backgroundColor: 'lightgreen',
+    padding: 10,
+    marginTop: 20,
   },
 });
--- ToDo_App_0120/components/Button.js
+++ ToDo_App_0120/components/Button.js
@@ -1,93 +1,20 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function Button({ label, theme }) {
-    if (theme === "primary") {
-        return (
-            <View
-            style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}
-            >
-                <Pressable
-                style={[styles.button, { backgroundColor: "#fff" }]}
-                onPress={() => alert('hmm...')}
-                >
-                    <FontAwesome
-                        size={18}
-                        color="#25292e"
-                    />
-                <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
-    else if(theme==="add") {
-      return (
-        <View
-        style={[styles.buttonContainerAdd, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}
-        >
-            <Pressable
-            style={[styles.buttonAdd, { backgroundColor: "#fff" }]}
-            onPress={() => alert('hmm...')}
-            >
-                <FontAwesome
-                    size={18}
-                    color="#25292e"
-                />
-            <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
-            </Pressable>
-        </View>
-    );
-    }
-    return (
-        <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('hmm...')}>
-              <Text style={styles.buttonLabel}>{label}</Text>
-            </Pressable>
-        </View>
-    );
-}
+const Button = ({ onPress, text }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text>{text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    width: 320,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 3,
-  },
-  buttonContainerAdd: {
-    width: 160,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: 'bottom',
-    justifyContent: 'left',
-    padding: 3,
-    position: 'fixed',
-    bottom: 0,
-  },
   button: {
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  buttonAdd: {
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  buttonLabel: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
+    padding: 10,
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
   },
 });
+
+export default Button;
